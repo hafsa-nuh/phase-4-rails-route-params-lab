f9a1c47f90a27a1092a24ff45ab45455ebd0d202
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2022-11-21 18:37:23 +0300

    done

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   get '/students', to: 'students#index'
   get '/students/grades', to: 'students#grades'
   get '/students/highest-grade', to: 'students#highest_grade'
+  
+  # student by id
+  get '/students/:id', to: 'students#show'
+
 end
